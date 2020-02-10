9c5d1f63ef041b788431be553ae6e1a09286799a
David <dchaneypin@student.rccd.edu>
2020-02-10 10:27:36 -0800

    Added some Base Scripts and Basic Game Manager

---
+++ GAM_24_Final/Assets/David/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Player;
+    [SerializeField]
+    private Text playerPoints;
+    [SerializeField]
+    private GameObject machineGun;
+    [SerializeField]
+    private GameObject rpgGun;
+    [SerializeField]
+    private GameObject sniperGun;
+    [SerializeField]
+    private GameObject pausePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void OnPauseMenu()
+    {
+        pausePanel.SetActive(true);
+    }
+
+    void OnLevelMenu()
+    {
+        SceneManager.LoadScene("LevelMenu");
+
+    }
+
+    void OnLevelOne()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+    void OnLevelTwo()
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
+
+}

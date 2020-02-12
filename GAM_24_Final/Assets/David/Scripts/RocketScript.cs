de66c2de42c59d52889fa6a223bb52237232186f
David Chaney <dchaneypin@student.rccd.edu>
2020-02-12 11:18:21 -0800

    added a projectile prefab and boundaries

---
+++ GAM_24_Final/Assets/David/Scripts/RocketScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour 
+{
+    [SerializeField]
+    private float force;
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+     //   transform.Translate = new (Vector3.forward);
+	}
+}

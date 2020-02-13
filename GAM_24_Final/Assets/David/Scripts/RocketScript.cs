aae902375002c70cf619c6b2505ce0e07c7b598d
David Chaney <dchaneypin@student.rccd.edu>
2020-02-13 01:20:07 -0800

    I put the  RPG and Sniper in Place, Im about to code them

--- GAM_24_Final/Assets/David/Scripts/RocketScript.cs
+++ GAM_24_Final/Assets/David/Scripts/RocketScript.cs
@@ -5,12 +5,41 @@ using UnityEngine;
 public class RocketScript : MonoBehaviour 
 {
     [SerializeField]
-    private float force;
+    private float startingVelocity = 20f;
+    [SerializeField]
+    private float radius = 10f;
+    private float force = 200f;
+    private float up = 500;
 
-	
-	// Update is called once per frame
-	void Update () 
+
+
+    public float damage = 10f;
+    public float delay  = 3f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-     //   transform.Translate = new (Vector3.forward);
-	}
+        Destroy(gameObject, delay);
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * startingVelocity;
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hit in hits)
+        {
+            if (hit.gameObject == gameObject)
+                continue;
+            Rigidbody rigidbody = hit.gameObject.GetComponent<Rigidbody>();
+
+            if(rigidbody !=null)
+                rigidbody.AddExplosionForce(force, transform.position, radius, up);
+        }
+        Destroy(gameObject);
+    }
+
+
+
 }
+

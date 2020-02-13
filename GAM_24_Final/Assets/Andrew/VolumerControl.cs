abe7032585e5dc342a1cd69118d5a649cfa6dbcd
Andrew sanchez <chiv3s4lif3@aol.com>
2020-02-13 09:52:46 -0800

    Music volume control

---
+++ GAM_24_Final/Assets/Andrew/VolumerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Slider slider = GetComponent<Slider>();
+
+        slider.value = GameObject.FindGameObjectWithTag("music").GetComponent<AudioSource>().volume;
+
+        slider.onValueChanged.AddListener(OnValueChanged);
+    }
+
+    void OnValueChanged(float volume)
+    {
+        Debug.Log("AUDIO CHANGED....");
+        GameObject.FindGameObjectWithTag("music").GetComponent<AudioSource>().volume = volume;
+    }
+}
